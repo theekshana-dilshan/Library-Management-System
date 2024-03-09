d1326d78101ddb11a5a9a3901fca7d5443038b66
theekshana-dilshan <sptheekshanadilshan@gmail.com>
2024-03-10 00:33:24 +0530

    updated

--- src/main/java/org/example/controller/BooksFormController.java
+++ src/main/java/org/example/controller/BooksFormController.java
@@ -1,4 +1,82 @@
 package org.example.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class BooksFormController {
+
+    @FXML
+    private JFXComboBox<?> cmbGenre;
+
+    @FXML
+    private TableColumn<?, ?> colAuthor;
+
+    @FXML
+    private TableColumn<?, ?> colAvailability;
+
+    @FXML
+    private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colGenre;
+
+    @FXML
+    private TableColumn<?, ?> colRemove;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableColumn<?, ?> colUpdate;
+
+    @FXML
+    private AnchorPane subRoot;
+
+    @FXML
+    private TableView<?> tblBooks;
+
+    @FXML
+    private JFXTextField txtAuthor;
+
+    @FXML
+    private JFXTextField txtBookId;
+
+    @FXML
+    private JFXTextField txtTitle;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/org/example/controller/BranchesFormController.java
+++ src/main/java/org/example/controller/BranchesFormController.java
@@ -1,4 +1,101 @@
 package org.example.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class BranchesFormController {
+
+    @FXML
+    private JFXComboBox<String> cmbStatus;
+
+    @FXML
+    private TableColumn<?, ?> colCode;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colLocation;
+
+    @FXML
+    private TableColumn<?, ?> colRemove;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableColumn<?, ?> colUpdate;
+
+    @FXML
+    private AnchorPane subRoot;
+
+    @FXML
+    private TableView<?> tblBanches;
+
+    @FXML
+    private JFXTextField txtBranchCode;
+
+    @FXML
+    private JFXTextField txtContact;
+
+    @FXML
+    private JFXTextField txtLocation;
+
+    @FXML
+    private JFXTextField txtSearch;
+
+    public void initialize(){
+        setComboBoxItems();
+    }
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
+
+    void setComboBoxItems(){
+        ObservableList<String> obList = FXCollections.observableArrayList();
+
+        String open = "Open";
+        String close = "Close";
+
+        obList.add(open);
+        obList.add(close);
+
+        cmbStatus.setItems(obList);
+    }
+
 }
+
